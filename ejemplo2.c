afdf80faf80708f90e9f51a9e98a8d92e7bad123
DianaSanC <dianacomipems@gmail.com>
2025-03-13 22:24:42 -0600

    ejemplos

--- ejemplo2.c
+++ ejemplo2.c
@@ -5,9 +5,10 @@ int main(){
     int *arreglo, num, cont;
     printf("¿Cuántos elementos tiene el conjunto?\n");
     scanf("%d", &num);
-    printf ("%p\n", &arreglo);
-
-    arreglo = (int*)calloc(num , sizeof(int));
+    printf("%d\n,arreglo");
+    
+    arreglo = (int*)calloc(num , sizeof(int)); // el primer elemento almacena el numero de elementos a reservar y el segundo el tamaño de los elementos a reservar
+    // regresar apuntador de tipo void callo si garantiza que se les asigne un 0 en cada una de las localidades de memoria que se reservaron 
     if(arreglo != NULL){
         printf
         ("Vector reservado:\n\t[");
@@ -16,7 +17,8 @@ int main(){
         }
         printf("\t]\n");
         printf("Liberando el espacio reservado\n");
-        free(arreglo);        
+        free(arreglo); 
+        arreglo = NULL;
     }
     return 0;
 }
--- ejemplo3.c
+++ ejemplo3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    int *arreglo=NULL, *arreglo2=NULL, num, cont;
+    int *arreglo=NULL, *arreglo2, num, cont;
     printf("¿Cuántos elementos tiene el conjunto?\n");
     scanf("%d", &num);
     arreglo = (int*)malloc(num *sizeof(int));
@@ -20,6 +20,8 @@ int main(){
         printf("Aumentando el espacio reservado al doble\n");
         num *=2;
         arreglo2 = (int*)realloc(arreglo, num*sizeof(int));
+        // modificar el tamaño de una memoria que ya se recibio
+        //apuntador de  argumento numero dos 
         if(arreglo2 != NULL){ 
             arreglo = arreglo2;       
             for(; cont <num; cont++){
@@ -30,20 +32,19 @@ int main(){
             for(cont =0; cont <num; cont++){
                 printf("\t%d", *(arreglo2 + cont));            
             }
-            printf("\t]\n");  
-            arreglo2 = (int*)realloc(arreglo, 2*sizeof(int));  
+            printf("\t]\n");    
+            // arreglo y arrerglo dos apunta al mismo
+            arreglo2 = (int*)realloc(arreglo, 2*sizeof(int));
              printf("Vector reservado:\n\t[");
-            for(cont =0; cont <2; cont++){
+            for(cont =0; cont <6; cont++){
                 printf("\t%d", *(arreglo2 + cont));            
             }
-            printf("\t]\n");  
-            arreglo2 = (int*)realloc(arreglo, 2*sizeof(int));  
-            
+            printf("\t]\n");   
         }
         printf("Liberando el espacio reservado\n");
-
-        free(arreglo); 
-        arreglo = arreglo2=NULL;       
+  free(arreglo);        
+        arreglo = arreglo2 =NULL;
+        // Puede disminuir el tamaño del arreglo
     }
     return 0;
 }
